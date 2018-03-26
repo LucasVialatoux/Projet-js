0313a641bb871a61bb8c6f8cba22056bdd3fb540
Vialatoux <lucas.vialatoux@yahoo.fr>
2018-03-26 17:49:38 +0200

    Continuation JS

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-/*var url = "http://crossorigin.me/http://www.podcastdirectory.com/podcasts/simon-b-the-moonproject-show-5262.html";
+var url = "http://crossorigin.me/http://rss.cnn.com/services/podcasting/studentnews/rss.xml";
 
 var data = new XMLHttpRequest();
 data.open('GET', url, true);
 
 // La réponse doit être de type String ou "document"
-//data.responseType = 'document';
+data.responseType = 'document';
 // La réponse sera parsée en xml
 data.overrideMimeType('text/xml');
 
@@ -17,13 +17,12 @@ data.onload = function () {
 };
 
 data.send(null);
-*/
+
 var v = new Array();
 
 v[0] = [
         "video.mp4",
-        "video2.mp4",
-        "video3.mp4"
+        "video2.mp4"
         ];
 
 var video = document.getElementById("VideoPlayer");
@@ -37,14 +36,46 @@ function pauseVideo() {
     video.pause(); 
 }
 
-function nextVideo() { 
-    video.pause(); 
+var n = 0;
+
+function nextVideo() {
+	
+	
+	if(n<v[0].length-1){
+		video.setAttribute("src", v[0][n+1]);
+		video.load(); 
+		n+=1;
+		console.log(n);
+	}
+	else{
+		video.setAttribute("src", v[0][0]);
+		video.load();
+		n=0;
+		console.log(n);
+	}
 }
 
 function previousVideo() { 
-    video.pause(); 
+    
+	
+	if(n>0){
+		video.setAttribute("src", v[0][n-1]);
+		video.load();
+		console.log(n);
+		n=n-1;
+	}
+	else{
+		var max=v[0].length-1;
+		
+		video.setAttribute("src", v[0][max]);
+		video.load();
+		n=max;
+		console.log(max);
+		console.log(n);
+	} 
 }
 
 function load(){
-	source.setAttribute("src", v[0][0]);
+	video.setAttribute("src", v[0][1]);
+	video.load();
 }
